34b83fb87d168b056402bc39759470f378d75675
Gustavo Narea <gustavo@valencia.(none)>
2011-05-02 20:26:03 +0100

    Updated trove classifier for development status (set to mature)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 #
 # Copyright (c) 2007, Agendaless Consulting and Contributors.
 # Copyright (c) 2008, Florent Aide <florent.aide@gmail.com>.
-# Copyright (c) 2008-2009, Gustavo Narea <me@gustavonarea.net>.
+# Copyright (c) 2008-2011, Gustavo Narea <me@gustavonarea.net>.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the BSD-like license at
@@ -28,7 +28,7 @@ setup(name='repoze.who.plugins.sa',
       description=('The repoze.who SQLAlchemy plugin'),
       long_description=README,
       classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 6 - Mature",
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
         "Natural Language :: English",
